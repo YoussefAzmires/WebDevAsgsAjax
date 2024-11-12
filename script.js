2c35baabc7ec59e1e0cbac87602e6994c7003de0
Youssef Azmires <youssef.azmires@yahoo.com>
2024-11-12 09:51:25 -0800

    commented

--- script.js
+++ script.js
@@ -1,31 +1,27 @@
- const url = `https://api.tvmaze.com/search/shows?q=${input}`
+const url = `https://api.tvmaze.com/search/shows?q=${input}`;
 
- const resultDiv = document.getElementById("results")
-async function  FetchData(){
-    try{
-        const response = await fetch(url);
-        if(!response.ok){
-            
-        }
-
-        const data = await response.json();
-
-        if(data.length===0)
-        {
-            resultDiv.innerHTML = "<p> No shows found </p>"
-        }
-
-
-        
-        
-
-    }
-    catch(error){
-        console.log(error);
+const resultDiv = document.getElementById("results");
+async function FetchData() {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      resultDiv.innerHTML = "<p> Error fetching data! No illegal shows for you tonight :) </p>";
     }
 
+    const data = await response.json();
+
+    if (data.length === 0) {
+      resultDiv.innerHTML = "<p> No shows found </p>";
+    }
+    DisplayInfo();
+  } catch (error) {
+    console.log(error);
+  }
 }
-
+function DisplayInfo() {
+  resultDiv.innerHTML = `<p>"Name:${data.show.name}</p>"`;
+  resultDiv.innerHTML = `<p>"Rating:${data.show.rating.average}</p>"`;
+  resultDiv.innerHTML = `<p>"Description:${data.show.summary}</p>"`;
+}
 //event listener when user click submit button
-document.getElementById("searchButton").addEventListener("click", FetchData);
-
+document.getElementById("searchInput").addEventListener("input", FetchData);
